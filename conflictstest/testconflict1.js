56b13b9f67815b0b1cee78b31089a663f40c32e8
Yu WANG <yuzi_801212@sina.com.cn>
2019-03-15 07:44:13 +0800

    Revert "commit 3 files together"
    This reverts commit 109994305c17c9017b3116511254847a4227995d.

--- conflictstest/testconflict1.js
+++ conflictstest/testconflict1.js
@@ -2,10 +2,8 @@ function f1(){
     console.log('jdfh kdfkdfk');
     console.log('from branch 1 again');
     console.log('from branch 1 again');
-<<<<<<< HEAD
+
     console.log('fgssss')
-=======
+
     console.log('kdsfdhfkkk');
->>>>>>> c5a710c4ed22ef9c929d308d6f52c30cfe826c2e
 }
-console.log('kjdhfkjdshkfdh')
--- conflictstest/testconflict2.js
+++ conflictstest/testconflict2.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
+
 console.log('3 files change')
-=======
-console.log('commit in merge')
->>>>>>> c5a710c4ed22ef9c929d308d6f52c30cfe826c2e
