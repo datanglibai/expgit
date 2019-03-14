109994305c17c9017b3116511254847a4227995d
Yu WANG <yuzi_801212@sina.com.cn>
2019-03-15 07:33:36 +0800

    commit 3 files together

--- conflictstest/testconflict1.js
+++ conflictstest/testconflict1.js
@@ -2,4 +2,6 @@ function f1(){
     console.log('jdfh kdfkdfk');
     console.log('from branch 1 again');
     console.log('from branch 1 again');
+    console.log('fgssss')
 }
+console.log('kjdhfkjdshkfdh')
