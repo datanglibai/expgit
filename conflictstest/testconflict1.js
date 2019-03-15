3d41287ad390b230f5e332d19b7e8d0f7ebb0cf2
Yu WANG <ywang38@slb.com>
2019-03-15 13:13:24 +0800

    add line

--- conflictstest/testconflict1.js
+++ conflictstest/testconflict1.js
@@ -1,9 +1,9 @@
-function f1(){
+function f1() {
     console.log('jdfh kdfkdfk');
     console.log('from branch 1 again');
     console.log('from branch 1 again');
 
     console.log('fgssss')
 
-    console.log('kdsfdhfkkk, dkjsfhsdjkhfdj');
+    console.log('kdsfdhfkkk');
 }
